11d6e19f62c695e8e4d9f625238c73307ed40a67
manwtein <manko.dmitry@outlook.com>
2018-09-17 02:44:50 +0300

    optimization

--- app/src/main/java/ru/startandroid/currencies/model/Valute.java
+++ app/src/main/java/ru/startandroid/currencies/model/Valute.java
@@ -7,7 +7,7 @@ import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
 
 @Root(name = "Valute", strict = false)
-public class Valute implements Parcelable{
+public class Valute {
 
 
     @Element(required = false, name = "CharCode")
@@ -15,13 +15,6 @@ public class Valute implements Parcelable{
 
     @Element(required = true, name = "Value")
     private String value;
-
-    public Valute(Parcel source) {
-        charCode = source.readString();
-        value = source.readString();
-    }
-
-    public Valute(){}
 
     public String getCharCode() {
         return charCode;
@@ -38,27 +31,4 @@ public class Valute implements Parcelable{
     public void setValue(String value) {
         this.value = value;
     }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(charCode);
-        dest.writeString(value);
-    }
-
-    public static Parcelable.Creator<Valute> CREATOR = new Creator<Valute>() {
-        @Override
-        public Valute createFromParcel(Parcel source) {
-            return new Valute(source);
-        }
-
-        @Override
-        public Valute[] newArray(int size) {
-            return new Valute[size];
-        }
-    };
 }
